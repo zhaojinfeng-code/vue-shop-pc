77ddef417c2dc581a00814da41d32c68034574b4
zhaojinfeng-code <30962620@qq.com>
2021-02-19 16:22:00 +0800

    登录功能开发完成

--- vue-shop/src/router/index.js
+++ vue-shop/src/router/index.js
@@ -1,29 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+ const router = new VueRouter({
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+	{ path: '/home', component: Home }]
 })
-
-export default router
+router.beforeEach((to,from,next)=>{
+	if(to.path=="/login") next()
+	//如果没有token直接返回到login页面，否则放行
+	const token = window.sessionStorage.getItem("token")
+	
+	if(!token) return next("/login")
+	next()
+	
+})
+export default router;
